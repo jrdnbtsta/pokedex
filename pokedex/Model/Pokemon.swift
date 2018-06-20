cf47acfc8cb9251fc94fa7fb7a90fa29ff38dc4b
Jordan Bautista <jordanbau@gmail.com>
2018-06-20 09:35:04 -0700

    fixed images and background / search bar / view autolayout

---
+++ pokedex/Model/Pokemon.swift
@@ -0,0 +1,27 @@
+//
+//  Pokemon.swift
+//  pokedex
+//
+//  Created by Jordan Bautista on 6/19/18.
+//  Copyright © 2018 Jordan Bautista. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+}
